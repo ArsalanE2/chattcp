d0cf71c82ffe75d1d6a9ba36c8d23809f446d57d
ArsalanE2 <104782535+ArsalanE2@users.noreply.github.com>
2023-03-07 16:08:15 +0100

    Add files via upload

---
+++ chatclient/chatclient/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class TCPClient
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", 8080);
+
+            Console.WriteLine("Connected to server");
+
+            NetworkStream stream = client.GetStream();
+            string message = "";
+
+            while (true)
+            {
+                Console.Write("You: ");
+                message = Console.ReadLine();
+
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                data = new byte[256];
+                int bytes = stream.Read(data, 0, data.Length);
+                message = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Server: {0}", message);
+            }
+
+            stream.Close();
+            client.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: {0}", e);
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ chatserver/chatserver/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TCPServer
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
+            server.Start();
+
+            Console.WriteLine("Server started");
+
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client connected");
+
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                byte[] data = new byte[256];
+                int bytes = stream.Read(data, 0, data.Length);
+                string message = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Client: {0}", message);
+
+                Console.Write("You: ");
+                message = Console.ReadLine();
+
+                data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+
+            stream.Close();
+            client.Close();
+            server.Stop();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: {0}", e);
+        }
+
+        Console.ReadKey();
+    }
+}
+
